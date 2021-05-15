7e22b504c33f242b874c7f04dcb150851d2e633e
İçimi Demirağ <icimidemirag@gmail.com>
2021-05-15 13:26:13 +0300

    Interface-Abstract (DAY 4 - HW1)

---
+++ InterfaceAbstractDemo/src/InterfaceAbstractDemo/Main.java
@@ -0,0 +1,19 @@
+package InterfaceAbstractDemo;
+
+import java.time.LocalDate;
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		customerManager.save(new Customer(1, "Ýçimi", "Demirað", LocalDate.of(2001,07,17), "12345678900"));
+	
+	}
+
+}
